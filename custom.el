f21705a431b6094b35d889a2eb125ac92f15961c
el-ethan <elethan@gmail.com>
2015-06-23 22:12:29 -0500

    Change customizations.el to standard custom.el

---
+++ custom.el
@@ -0,0 +1,17 @@
+;; Destination for settings made through Customize
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (misterioso)))
+ '(inhibit-default-init nil)
+ '(inhibit-startup-buffer-menu t)
+ '(inhibit-startup-screen t)
+ '(safe-local-variable-values (quote ((buffer-auto-save-file-name)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
