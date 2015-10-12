fa23df270abb6f3a2b84367abebda727de886c12
el-ethan <elethan@gmail.com>
2015-10-11 21:40:34 -0500

    Add some theme stuff

--- custom.el
+++ custom.el
@@ -4,12 +4,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (misterioso)))
+ '(custom-enabled-themes (quote (aurora)))
+ '(custom-safe-themes
+   (quote
+    ("91faf348ce7c8aa9ec8e2b3885394263da98ace3defb23f07e0ba0a76d427d46" "1131bccbdb60c05d70ed23af0c3e5d7dd6a64c4ba5d1f9794b56516bc8dd7c5f" "76626efc044daee1c402e50f185bd633d1a688c332bc15c8fd5db4cdf2966b79" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "877530ef4d4423b5b184daff52953d398bb3533ec5e7393c238ac732b19135dd" default)))
  '(inhibit-default-init nil)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-screen t)
  '(line-move-visual nil)
- '(org-modules (quote (org-docview org-habit org-info org-mhe org-w3m)))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-modules
+   (quote
+    (org-docview org-habit org-info org-mhe org-w3m org-drill)))
  '(safe-local-variable-values (quote ((buffer-auto-save-file-name)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
