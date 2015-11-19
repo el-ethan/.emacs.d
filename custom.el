61c4d96b5926e5e8a659e48ddbe45fb57ef20c7d
ethan <ethan@essential-elements.net>
2015-11-19 08:37:57 -0600

    Ubdate custom.el and pdb snippet

--- custom.el
+++ custom.el
@@ -8,6 +8,8 @@
  '(custom-safe-themes
    (quote
     ("790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "91faf348ce7c8aa9ec8e2b3885394263da98ace3defb23f07e0ba0a76d427d46" "1131bccbdb60c05d70ed23af0c3e5d7dd6a64c4ba5d1f9794b56516bc8dd7c5f" "76626efc044daee1c402e50f185bd633d1a688c332bc15c8fd5db4cdf2966b79" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "877530ef4d4423b5b184daff52953d398bb3533ec5e7393c238ac732b19135dd" default)))
+ '(elpy-project-root-finder-functions (quote (elpy-project-find-projectile-root)))
+ '(helm-locate-project-list (quote ("/home/ethan/git")))
  '(inhibit-default-init nil)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-screen t)
