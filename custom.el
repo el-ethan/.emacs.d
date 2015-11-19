365a2f57b08cbef8f1102442b977b6c5537af2b4
el-ethan <elethan@gmail.com>
2015-11-18 22:00:38 -0600

    Add lisp code for work

--- custom.el
+++ custom.el
@@ -4,10 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
  '(custom-enabled-themes (quote (aurora)))
  '(custom-safe-themes
    (quote
-    ("790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "91faf348ce7c8aa9ec8e2b3885394263da98ace3defb23f07e0ba0a76d427d46" "1131bccbdb60c05d70ed23af0c3e5d7dd6a64c4ba5d1f9794b56516bc8dd7c5f" "76626efc044daee1c402e50f185bd633d1a688c332bc15c8fd5db4cdf2966b79" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "877530ef4d4423b5b184daff52953d398bb3533ec5e7393c238ac732b19135dd" default)))
+    ("3e72e20b462954ec339684cb4d0d2d71293f4b8b1564dde1fc94c818dc427ed3" "54159ea82516378faa7c4d25fb549b843effb1eb932f0925dce1348de7a659ba" "d3df47c843c22d8f0177fe3385ae95583dc8811bd6968240f7da42fd9aa51b0b" "c86f868347919095aa44d2a6129dd714cbcf8feaa88ba954f636295b14ceff8f" "8577da1641ed4bdf255341ca92e3d0e49c9f4d574458f09ce78159690442cade" "3ed645b3c08080a43a2a15e5768b893c27f6a02ca3282576e3bc09f3d9fa3aaa" "790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "91faf348ce7c8aa9ec8e2b3885394263da98ace3defb23f07e0ba0a76d427d46" "1131bccbdb60c05d70ed23af0c3e5d7dd6a64c4ba5d1f9794b56516bc8dd7c5f" "76626efc044daee1c402e50f185bd633d1a688c332bc15c8fd5db4cdf2966b79" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "877530ef4d4423b5b184daff52953d398bb3533ec5e7393c238ac732b19135dd" default)))
+ '(hl-sexp-background-color "#1c1f26")
  '(inhibit-default-init nil)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-screen t)
@@ -15,9 +18,7 @@
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
- '(org-modules
-   (quote
-    (org-docview org-habit org-info org-mhe org-w3m org-drill)))
+ '(org-modules (quote (org-docview org-habit org-info org-mhe org-w3m)))
  '(python-check-command "/usr/local/bin/pyflakes")
  '(safe-local-variable-values (quote ((buffer-auto-save-file-name)))))
 (custom-set-faces
---
+++ work.el
@@ -0,0 +1,5 @@
+;; Stuff related to work
+
+;; Set PYTHONPATH for aeris project
+(setenv "PYTHONPATH" "/home/ethan/git/aeris2:/home/ethan/git/kivy:/home/ethan/git/gryphus:/home/ethan/git/ee-sentinel:/home/ethan/git/ee-updater")
+
