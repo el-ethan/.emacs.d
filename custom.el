8740891305d4faa4152a2d08070116b7f075c82e
ethan <ethan@essential-elements.net>
2015-11-20 08:48:01 -0600

    Change custom

--- custom.el
+++ custom.el
@@ -6,10 +6,10 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
- '(custom-enabled-themes (quote (misterioso)))
+ '(custom-enabled-themes (quote (atom-one-dark ample wombat misterioso)))
  '(custom-safe-themes
    (quote
-    ("3e72e20b462954ec339684cb4d0d2d71293f4b8b1564dde1fc94c818dc427ed3" "54159ea82516378faa7c4d25fb549b843effb1eb932f0925dce1348de7a659ba" "d3df47c843c22d8f0177fe3385ae95583dc8811bd6968240f7da42fd9aa51b0b" "c86f868347919095aa44d2a6129dd714cbcf8feaa88ba954f636295b14ceff8f" "8577da1641ed4bdf255341ca92e3d0e49c9f4d574458f09ce78159690442cade" "3ed645b3c08080a43a2a15e5768b893c27f6a02ca3282576e3bc09f3d9fa3aaa" "790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "91faf348ce7c8aa9ec8e2b3885394263da98ace3defb23f07e0ba0a76d427d46" "1131bccbdb60c05d70ed23af0c3e5d7dd6a64c4ba5d1f9794b56516bc8dd7c5f" "76626efc044daee1c402e50f185bd633d1a688c332bc15c8fd5db4cdf2966b79" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "877530ef4d4423b5b184daff52953d398bb3533ec5e7393c238ac732b19135dd" default)))
+    ("4904daa168519536b08ca4655d798ca0fb50d3545e6244cefcf7d0c7b338af7e" "3e72e20b462954ec339684cb4d0d2d71293f4b8b1564dde1fc94c818dc427ed3" "4f5bb895d88b6fe6a983e63429f154b8d939b4a8c581956493783b2515e22d6d" default)))
  '(fci-rule-color "#232A2F")
  '(hl-sexp-background-color "#1c1f26")
  '(inhibit-default-init nil)
