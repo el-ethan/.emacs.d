04ccab0f60fa6552e2edcb5ada181f368533be8c
ethan <ethan@essential-elements.net>
2015-11-24 08:59:59 -0600

    Change some stuff like fill-column

--- work.el
+++ work.el
@@ -1,6 +1,8 @@
 
 ;; Stuff related to work
 
+(setq-default fill-column 120)
+
 ;; Set PYTHONPATH for aeris project
-;; (setenv "PYTHONPATH" "/home/ethan/git/aeris2:/home/ethan/git/kivy:/home/ethan/git/gryphus:/home/ethan/git/ee-sentinel:/home/ethan/git/ee-updater")
+;;(setenv "PYTHONPATH" "/home/ethan/git/aeris2:/home/ethan/git/kivy:/home/ethan/git/gryphus:/home/ethan/git/ee-sentinel:/home/ethan/git/ee-updater")
 
