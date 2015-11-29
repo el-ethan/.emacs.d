3d5060ce7ddf7abb11a6ee9e95f7e7c5ac250b31
el-ethan <elethan@gmail.com>
2015-11-28 20:30:32 -0600

    Change stuff in custom

--- custom.el
+++ custom.el
@@ -23,8 +23,9 @@
  '(projectile-globally-ignored-file-suffixes (quote ("png" "pyc" "elc")))
  '(python-check-command "/usr/local/bin/pyflakes")
  '(safe-local-variable-values (quote ((encoding . utf-8) (buffer-auto-save-file-name))))
- '(split-width-threshold nil)
- '(split-height-threshold 80)
+ ;; '(split-height-threshold 80)
+ ;; '(split-width-threshold nil)
+ '(term-completion-autolist nil)
  '(vc-annotate-background "#2B3B40")
  '(vc-annotate-color-map
    (quote
