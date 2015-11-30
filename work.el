6208dd9eb5b318e219a24c370cd5d844919b741b
ethan <ethan@essential-elements.net>
2015-11-30 16:50:22 -0600

    Change config

--- work.el
+++ work.el
@@ -2,6 +2,11 @@
 ;; Stuff related to work
 
 (setq-default fill-column 120)
+(setq gud-chdir-before-run nil)
+;; (defadvice
+;;     pdb
+;;     (before (lambda () (cd "/home/ethan/git/aeris2") activate))) 
+;; (add-hook 'gud-mode-hook (lambda () (cd "/home/ethan/git/aeris2")))
 
 ;; Set PYTHONPATH for aeris project
 ;;(setenv "PYTHONPATH" "/home/ethan/git/aeris2:/home/ethan/git/kivy:/home/ethan/git/gryphus:/home/ethan/git/ee-sentinel:/home/ethan/git/ee-updater")
