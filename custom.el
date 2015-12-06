7c43b6ba62a94df25fc503f2725bb8ef0bc71d61
el-ethan <elethan@gmail.com>
2015-12-06 14:00:11 -0600

    Update theme stuff

--- custom.el
+++ custom.el
@@ -55,6 +55,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight-indentation-face ((t nil)))
  '(kivy-tab-face ((t nil)))
  '(term-color-blue ((t (:background "#61AFEF" :foreground "#61AFEF"))))
  '(term-color-green ((t (:background "#98C379" :foreground "#98C379"))))
