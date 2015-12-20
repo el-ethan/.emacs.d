efce46b2ff42ae540f8c19af5e10b83632859340
el-ethan <elethan@gmail.com>
2015-12-20 14:11:04 -0600

    Add column-number-mode to minimal-init

--- minimal-init.el
+++ minimal-init.el
@@ -1,5 +1,7 @@
 ;; Minimal configuration for editing commit messages, etc.
 (add-to-list 'auto-mode-alist '("\\COMMIT_EDITMSG\\'" . text-mode))
+;(load-theme 'wombat t)
+(column-number-mode)
 (flyspell-mode)
 (add-hook 'text-mode-hook 'flyspell-mode)
 
