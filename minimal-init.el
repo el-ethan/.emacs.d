f07197b8972d60922f2c9d05d2ac4811f96ce0fa
el-ethan <elethan@gmail.com>
2015-12-20 21:21:37 -0600

    Add theme to minimal config

--- minimal-init.el
+++ minimal-init.el
@@ -1,6 +1,6 @@
 ;; Minimal configuration for editing commit messages, etc.
 (add-to-list 'auto-mode-alist '("\\COMMIT_EDITMSG\\'" . text-mode))
-;(load-theme 'wombat t)
+(load-theme 'misterioso t)
 (column-number-mode)
 (flyspell-mode)
 (add-hook 'text-mode-hook 'flyspell-mode)
