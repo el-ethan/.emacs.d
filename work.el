6757e950f7e6fbb63197c449476b98dcd7e79271
ethan <ethan@essential-elements.net>
2015-12-29 11:52:48 -0600

    Test commit

--- work.el
+++ work.el
@@ -10,9 +10,8 @@
 (setq gud-chdir-before-run nil)
 ;; (defadvice
 ;;     pdb
-;;     (before (lambda () (cd "/home/ethan/git/aeris2") activate))) 
+;;     (before (lambda () (cd "/home/ethan/git/aeris2") activate)))
 ;; (add-hook 'gud-mode-hook (lambda () (cd "/home/ethan/git/aeris2")))
-
+;;
 ;; Set PYTHONPATH for aeris project
 ;;(setenv "PYTHONPATH" "/home/ethan/git/aeris2:/home/ethan/git/kivy:/home/ethan/git/gryphus:/home/ethan/git/ee-sentinel:/home/ethan/git/ee-updater")
-
