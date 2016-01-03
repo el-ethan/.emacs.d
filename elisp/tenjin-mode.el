33b52e26197fca3ed3d7cc05741b498bf20cbad2
el-ethan <elethan@gmail.com>
2016-01-02 21:46:28 -0600

    Update tenjin-mode

--- elisp/tenjin-mode.el
+++ elisp/tenjin-mode.el
@@ -1,17 +1,14 @@
 ;; Define keywords
-(setq tenjin-keywords '("if" "in" "for" "while" "is" "not" "or" "and" "<\\?py"))
-(setq tenjin-hidden-keywords '("<?py" "?>"))
+(setq tenjin-keywords '("if" "else" "elif" "in" "for" "while" "is" "not" "or" "and"))
 
 ;; Generate regex for keywords
 (setq tenjin-keyword-regexp (regexp-opt tenjin-keywords 'words))
-(setq tenjin-hidden-keywords-regexp (regexp-opt tenjin-hidden-keywords 'words))
 
 ;; create the list for font-lock.
 ;; each category of keyword is given a particular face
 (setq tenjin-font-lock-keywords
-    `(
-         (,tenjin-keyword-regexp . font-lock-keyword-face)
-         (,tenjin-hidden-keywords-regexp . font-lock-function-face)
+    `((,tenjin-keyword-regexp . font-lock-keyword-face)
+         ("\\?>\\|<\\?py" . font-lock-comment-face)
          ))
 
 (define-derived-mode tenjin-mode html-mode
@@ -19,5 +16,7 @@
   (setq mode-name "tenjin"))
 
 ;; clear out the unneeded regex?
+(setq tenjin-keywords nil)
+(setq tenjin-keywords-regexp nil)
 
 (provide 'tenjin-mode)
