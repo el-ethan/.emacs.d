c75db5f2d7095dd628623902ef3a19e31fa961d7
ethan <ethan@essential-elements.net>
2016-02-15 09:36:39 -0600

    Changes to custom by smart-mode-line

--- custom.el
+++ custom.el
@@ -4,11 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(custom-safe-themes
-   (quote
-    ("4904daa168519536b08ca4655d798ca0fb50d3545e6244cefcf7d0c7b338af7e" "3e72e20b462954ec339684cb4d0d2d71293f4b8b1564dde1fc94c818dc427ed3" "4f5bb895d88b6fe6a983e63429f154b8d939b4a8c581956493783b2515e22d6d" default)))
+    '(ansi-color-faces-vector
+         [default bold shadow italic underline bold bold-italic bold])
+    '(ansi-color-names-vector
+         ["#263238" "#74CBC4" "#546D7A" "#C792EA" "#82B1FF" "#FFCB6B" "#FF516D" "#CDD3D3"])
+    '(custom-safe-themes
+         (quote
+             ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "4904daa168519536b08ca4655d798ca0fb50d3545e6244cefcf7d0c7b338af7e" "3e72e20b462954ec339684cb4d0d2d71293f4b8b1564dde1fc94c818dc427ed3" "4f5bb895d88b6fe6a983e63429f154b8d939b4a8c581956493783b2515e22d6d" default)))
  '(fci-rule-color "#232A2F")
  '(git-gutter+-hide-gutter nil)
  '(gud-chdir-before-run nil t)
@@ -18,9 +20,9 @@
  '(inhibit-startup-screen t)
  '(line-move-visual nil)
  '(linum-eager nil)
- '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+    '(nrepl-message-colors
+         (quote
+             ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(org-modules (quote (org-docview org-habit org-info org-mhe org-w3m)))
  '(projectile-globally-ignored-file-suffixes (quote ("png" "pyc" "elc")))
  '(python-check-command "/usr/local/bin/pyflakes")
@@ -28,26 +30,26 @@
  '(split-width-threshold nil)
  '(term-completion-autolist nil)
  '(vc-annotate-background "#2B3B40")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#74CBC4")
-     (40 . "#74CBC4")
-     (60 . "#C2E982")
-     (80 . "#FFC400")
-     (100 . "#C792EA")
-     (120 . "#C792EA")
-     (140 . "#546D7A")
-     (160 . "#546D7A")
-     (180 . "#FF516D")
-     (200 . "#9FC59F")
-     (220 . "#859900")
-     (240 . "#F77669")
-     (260 . "#FF516D")
-     (280 . "#82B1FF")
-     (300 . "#82B1FF")
-     (320 . "#82B1FF")
-     (340 . "#D9F5DD")
-     (360 . "#FFCB6B"))))
+    '(vc-annotate-color-map
+         (quote
+             ((20 . "#74CBC4")
+                 (40 . "#74CBC4")
+                 (60 . "#C2E982")
+                 (80 . "#FFC400")
+                 (100 . "#C792EA")
+                 (120 . "#C792EA")
+                 (140 . "#546D7A")
+                 (160 . "#546D7A")
+                 (180 . "#FF516D")
+                 (200 . "#9FC59F")
+                 (220 . "#859900")
+                 (240 . "#F77669")
+                 (260 . "#FF516D")
+                 (280 . "#82B1FF")
+                 (300 . "#82B1FF")
+                 (320 . "#82B1FF")
+                 (340 . "#D9F5DD")
+                 (360 . "#FFCB6B"))))
  '(vc-annotate-very-old-color "#FFCB6B")
  '(yaml-indent-offset 4))
 (custom-set-faces
